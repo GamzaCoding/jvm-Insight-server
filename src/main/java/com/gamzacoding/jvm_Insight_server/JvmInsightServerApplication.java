750ddaf757fe8dcc0c2a7a25cb35b240354ee74a
HyeongSeok <hyeongseok1995@gmail.com>
2026-02-20 21:07:30 +0900

    Initial commit

---
+++ src/main/java/com/gamzacoding/jvm_Insight_server/JvmInsightServerApplication.java
@@ -0,0 +1,13 @@
+package com.gamzacoding.jvm_Insight_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmInsightServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvmInsightServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamzacoding/jvm_Insight_server/JvmInsightServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamzacoding.jvm_Insight_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmInsightServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
