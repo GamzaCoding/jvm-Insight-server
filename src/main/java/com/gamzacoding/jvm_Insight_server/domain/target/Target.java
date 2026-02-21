28b987f61ab02ce61072292d33ea6486176cd7e8
HyeongSeok <hyeongseok1995@gmail.com>
2026-02-21 22:50:41 +0900

    fix: db 테이블 내 NotNull 조건 관련 애러 해결, DB 의존성 문제 해결

--- src/main/java/com/gamzacoding/jvm_Insight_server/domain/target/Target.java
+++ src/main/java/com/gamzacoding/jvm_Insight_server/domain/target/Target.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import java.time.Instant;
 import lombok.Getter;
@@ -28,6 +29,13 @@ public class Target {
 
     @Column(name = "created_at", nullable = false)
     private Instant createdAt;
+
+    @PrePersist
+    void prePersist() {
+        if (this.createdAt == null) {
+            this.createdAt = Instant.now();
+        }
+    }
 
     public Target(int pid, String displayName) {
         this.pid = pid;
